6c93444853eebcb99e75ab013868a272952fc8f6
UNIDRA <57227914+UNIDRA@users.noreply.github.com>
2022-10-13 16:06:43 -0700

    q2
    Added the getinput, findmin, and main functions

--- q2.cpp
+++ q2.cpp
@@ -6,6 +6,32 @@ using namespace std;
 // Your code here
 // ******************************
 
+void getInput(int &n1, int &n2, int &n3) {
+	cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int n1, int n2, int n3) {
+	int min = 100;
+	if (min > n1) {
+        min = n1;
+    }
+    if (min > n2) {
+        min = n2;
+    }
+    if (min > n3) {
+        min = n3;
+    }
+}
+
+void printResult(int n1, int n2, int n3, int min);
+
+int main () {
+	int n1, n2, n3, min;
+	getInput(n1, n2, n3);
+	min = findMin(n1, n2, n3);
+	printResult(n1, n2, n3, min);
+	return 0;
+}
 // ******************************
 // this function will help you pass the test. When you print the result with this function, you will get the same output format
 void printResult(int n1, int n2, int n3, int min)
